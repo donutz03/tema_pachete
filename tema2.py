d528de007ecf72a96aac6fd08ca37553c60d306a
donutz03 <ionut.hodoroaga20@gmail.com>
2025-03-05 15:26:29 +0200

    tema partial

---
+++ tema2.py
@@ -0,0 +1,74 @@
+comenzi = [[1,'nume1', ['uscator','mar','masina'],2377,"Finalizata"],
+           [15,'nume2', ['para','fixativ','masina de spalat', 'stup'],9999,"Anulata"],
+            [23,'nume3', ['calculator','monitor'],1766,"In curs"],
+            [88,'nume4', ['mouse', 'tastatura','birou', 'scaun'],72377,"Finalizata"],
+            [11,'nume5', ['unitate','tabla','marker', 'pizza', 'cafea'],6682,"In curs"]
+           ]
+
+statusuri = ("Finalizata", "Anulata", "In curs")
+
+"""
+Extrage toate comenzile care au statusul "Finalizată".
+"""
+def extrage_finalizate():
+    finalizate = []
+    for comanda in comenzi:
+        if comanda[4]=="Finalizata":
+            finalizate.append(comanda)
+    return finalizate
+
+"""
+Calculează suma totală a comenzilor care sunt "Finalizate".
+"""
+def suma_finalizate():
+    suma = 0
+    for comanda in comenzi:
+        if comanda[4]=='Finalizata':
+            suma += comanda[3]
+    return suma
+
+"""
+Găsește comanda cu cel mai mare total de plată.
+"""
+def maxim_de_plata():
+    maxim = 0
+    for comanda in comenzi:
+        if comanda[3] > maxim:
+            maxim = comanda[3]
+    return maxim
+
+"""
+Sortează comenzile în funcție de totalul comenzii, în ordine crescătoare.
+"""
+def sorteaza_comenzi_dupa_total_descrescator():
+    lista_sortata_desc = [i for i in comenzi]
+    lista_sortata_desc.sort(key= lambda element : element[3], reverse=True)
+    return lista_sortata_desc
+
+"""
+Schimbă statusul comenzii cu un anumit ID (de exemplu, ID-ul 3) la "Anulată".
+"""
+def schimba_status_comanda(id_comanda, status_nou):
+    comanda_de_schimbat = []
+    if status_nou not in statusuri:
+        return "status incorect"
+    else:
+        for comanda in comenzi:
+            if comanda[0] == id_comanda:
+                comanda[4] = status_nou
+                comanda_de_schimbat = comanda
+                break
+    return comanda_de_schimbat
+
+"""
+Adaugă o nouă comandă în lista de comenzi.
+"""
+def adauga_comanda_in_lista(comanda):
+
+
+print(extrage_finalizate())
+print(suma_finalizate())
+print(maxim_de_plata())
+print(sorteaza_comenzi_dupa_total_descrescator())
+print(schimba_status_comanda(2, "lalala"))
+print(schimba_status_comanda(1, "Anulata"))
