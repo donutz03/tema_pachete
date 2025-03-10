65ddb9e78ef04d6e20b69bfd011b0973346595b7
donutz03 <ionut.hodoroaga20@gmail.com>
2025-03-10 12:29:01 +0200

    tema data viitoare

--- proiect/descriere_date.py
+++ proiect/descriere_date.py
@@ -1,7 +1,8 @@
 #Tema: Metode de tratare a valorilor lipsa (3 metode). Grupari pe date. Clusterizare dupa zona pentru harta
 #Surprinderea valorilor extreme si tratarea lor. Sa schimbam graficul (de corelatie) cu seaborn. Buguri de fixat
 #Analize de DSAD (Factorial, Clusterizare, ACP, Discriminanta)
-
+#Sa folosim BoxPlot. Sa pot da click pe o coloana ca sa iasa boxplot/figura.
+#Grupari de coloane
 #Modularizare : definire de functie (ca e un singur fisier cu tot codul)
 import streamlit as st
 import pandas as pd
