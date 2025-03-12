17d3ba52053702ef45787fe18e341897d7b5d142
donutz03 <ionut.hodoroaga20@gmail.com>
2025-03-12 14:33:54 +0200

    gata ex1 si 2

---
+++ tema_seminar_3_doua_exercitii/main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+df = pd.read_csv('clienti_leasing.csv')
+
+df_nou = df.loc[(df['VAL_CREDITS_RON'] == 0) & (df['DEPOSIT_AMOUNT'] > 150000),
+                ['NAME_CLIENT', 'DEPOSIT_AMOUNT', 'PRESCORING']]
+
+df_nou.loc[df_nou['DEPOSIT_AMOUNT'] > 500000, 'PRESCORING'] = 6
+
+df_nou.to_csv('rezultat_ex1.csv', index=False)
+print(df_nou)
+
+import json
+
+with open('clienti_daune.json') as f:
+    data = json.load(f)
+
+lista_cuvinte = []
+for dauna in data:
+    lista_cuvinte.extend(str(dauna['Dauna']).lower().split())
+
+dictionar = {}
+for cuvant in lista_cuvinte:
+    dictionar[cuvant] = dictionar.get(cuvant, 0) + 1
+
+cuvinte_eliminate = ['the', 'and', 'to', 'a']
+
+rezultat = []
+for cuvant, frecventa in dictionar.items():
+    if frecventa > 1000 and cuvant not in cuvinte_eliminate:
+        rezultat.append((frecventa, cuvant))
+
+rezultat.sort(reverse=True)
+print(rezultat)
